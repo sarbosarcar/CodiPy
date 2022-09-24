26bf9c637d5986b0bfa9f63a05c2a52c18becc54
Sarbo Sarcar <70145448+SarboSarcar@users.noreply.github.com>
2022-09-25 01:09:16 +0530

    Add core files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# Codexed
+## A GUI-based code generation platform built with OpenAI API using the Codex model
+#### *Constructed in partial fulfilment of the CS year-end project 2022-23*
+
+## Prerequisites
++ Python (preferably v3.9+) [see list of dependencies](requirements.txt)
++ Browser (preferably Chrome, Edge, Opera, Firefox or Safari)
++ Internet connectivity
+
+## Access
+
++ Download the project file [here](https://github.com/SarboSarcar/Flask-CS-proj/archive/refs/heads/main.zip)
++ View deployed version at *coming soon*
+
+## Usage
++ Once the server is up and running, visit the concerned routes and follow the instructions provided on the page. The GUI includes a login/signup page, the main query page and an about page.
+
+## Contributors
+
+## Acknowledgements
+
++ Bootstrap for the main jumbotron theme
++ Darkmodejs for the dark mode toggle button
++ Highlightjs for the syntax highlighting
+
+## License
+
++ Code open-sourced under the [MIT License](LICENSE)
+
+
---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request
+from flask.templating import render_template
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.get('/query')
+def get_query():
+    command = '''The code generated will be displayed here. This is some sample
+              text used for filling the space.'''
+    return render_template('query.html', command=command)
+
+
+@app.post('/query_t')
+def post_query():
+    lang = request.form.get('lang')
+    command = request.form.get('command').lower()
+    query = f'write a program in {lang} to {command}'
+    # print(query)
+    # print(request.form)
+    import os
+    import openai
+
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+    response = openai.Completion.create(
+        model="code-cushman-001",
+        prompt=f"{query}",
+        temperature=0,
+        max_tokens=2000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    ).choices[0].text
+
+    return render_template('query.html', response=response, command=command)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
